dcb7464501b8bbd1333564b9fc1a4eba725254ee
Beckham Ochieng <o.becks45@gmail.com>
2023-06-01 11:51:03 +0300

    Updated local-dev-packages

--- proxima_core_engine/local_dev_packages/kafka-connect-api/src/kafka_connect/connectors/__init__.py
+++ proxima_core_engine/local_dev_packages/kafka-connect-api/src/kafka_connect/connectors/__init__.py
@@ -3,5 +3,6 @@ from typing import Any
 from .base_connector import BaseJdbcFactory
 from .base_sink import BaseSink, BaseSinkConfig
 from .base_source import BaseSource, BaseSourceConfig
+from .base_es_sink import BaseEsSink, BaseEsSinkConfig
 
 Connectors = dict[str, dict[str, Any]]
