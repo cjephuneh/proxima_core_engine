ae427e301391b4da63afe5c2a4e748e348b014a9
Beckham Ochieng <o.becks45@gmail.com>
2023-06-02 09:06:05 +0300

    Updates

--- proxima_core_engine/local_dev_packages/ibl-kafka-connect-api/src/kafka_connect/connect_api/__init__.py
+++
@@ -1,1 +0,0 @@
-from .connect_api import KafkaConnectApi, KafkaResponse
--- proxima_core_engine/local_dev_packages/ibl-kafka-connect-api/src/kafka_connect/connectors/__init__.py
+++
@@ -1,8 +0,0 @@
-from typing import Any
-
-from .base_connector import BaseJdbcFactory
-from .base_sink import BaseSink, BaseSinkConfig
-from .base_source import BaseSource, BaseSourceConfig
-from .base_es_sink import BaseEsSink, BaseEsSinkConfig
-
-Connectors = dict[str, dict[str, Any]]
